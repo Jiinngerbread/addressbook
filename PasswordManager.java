af9dfa5733bd9d145eeb70e17ead1912d9e9280b
Jiinngerbread <jiinngerbread@gmail.com>
2019-04-14 04:49:12 -0500

    Update PasswordManager.java

--- PasswordManager.java
+++ PasswordManager.java
@@ -71,7 +71,7 @@ public class PasswordManager
         catch(InvalidKeySpecException ikeyEx) 
         {
             System.err.println("Exception occured in getEncryptedPassword()");
-            return null;
+            return false;
         }
     }
 }
