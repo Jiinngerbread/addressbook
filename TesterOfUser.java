3875a8fe5c647c9f729371d567cb307302d7f02c
Jiinngerbread <jiinngerbread@gmail.com>
2019-04-14 12:07:17 -0500

    Update TesterOfUser.java

--- TesterOfUser.java
+++ TesterOfUser.java
@@ -22,15 +22,15 @@ public class TesterOfUser
   	System.out.println("This is the password of the user in byte code: " + user1.getpassword());
     System.out.println("This is the salt for the user in byte code: " + user1.getSalt());
   	//String s = new String(user1.getpassword());
-    String s = new String(String.valueOf(user1.getpassword()));
+    /*String s = new String(String.valueOf(user1.getpassword()));
   	System.out.println("This is the password of the user from byte code to string: " + s);
     System.out.println();
     boolean result = user1.getpassword().authenticationCheck("Pencil12Case", user1.getpassword(), user1.getSalt());
   	System.out.println("This is the result when the password is the same : " + result);
     System.out.println();
     boolean result1 = user1.authenticationCheck("Pencil13Case", user1.getpassword(), user1.getSalt());
-    System.out.println("This is the result when the password is different : " + result1);
-
+    System.out.println("This is the result when the password is different : " + result1);*/
+    
 
   	/*User user2 = new User("Brandon", "Lumsden", Gender.MALE, 19900918, "BrandonBest", "123Passcode546");
   	System.out.println("This is the name of the user: " + user2.getName());
