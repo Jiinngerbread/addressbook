e82531ec274f3a22f2d4ae11eb23b9001de24ab4
Jiinngerbread <39425899+Jiinngerbread@users.noreply.github.com>
2019-04-14 03:49:13 -0500

    Update ContactList.java

--- ContactList.java
+++ ContactList.java
@@ -1,10 +1,16 @@
-public class ContactList extends Contact
+//This file shall utilize the FileManager class in an a strong agregation type relationship 
+//Sunch that we can read information from a a specific contact file 
+
+public class ContactList extends FileManager
 {
-  
-   private ArrayList <Contact> ContactList = new ArrayList<Contact>();
-   private String[] allInfo ;
+	private FileManager contactList = new FileManager(String path) //to be fleshed out better 
+	private ArrayList <Contact> ContactList = new ArrayList<Contact>();
+	private String[] allInfo ;
     
+	
     public ContactFileManager(String filename)
+	//to revisit and revise post tutorial 
+	//file name would be same as path here
 	  {
 		try 
 		{	
