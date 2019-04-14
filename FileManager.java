9b2e1912bc37166fbfba624fe04e91e24a75edea
Jiinngerbread <jiinngerbread@gmail.com>
2019-04-14 03:15:50 -0500

    Create FileManager.java

---
+++ FileManager.java
@@ -0,0 +1,26 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileManager
+{
+	//Check if there exists a folder in directory
+	// specific Users folder ie Addressbook user && specific user folder ie James 
+
+	// We want to create the create the Users folder
+	// We want to create a folder for a user in the Users folder 
+
+
+	// We want to check if a file exists
+
+	// We want to create a file in a specific destination folder 
+	// we want to write to the file 
+
+	//we want to edit the file /manipulate the file 
+
+	
+
+	
+}
