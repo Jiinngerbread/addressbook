8f388614ceb1a51cd77eab34d5fbaa6a8c2837ef
Jiinngerbread <jiinngerbread@gmail.com>
2019-04-14 19:37:27 -0500

    Update AddressBook.java

--- AddressBook.java
+++ AddressBook.java
@@ -1,8 +1,7 @@
 //package contact;
 
 import java.util.*;
-import java.time.LocalDate;
-import java.time.Period;
+
 
 /**
 * This class manages the contacts.
@@ -24,29 +23,5 @@ public class AddressBook
 
 
 
-	public static void main(String[] args)
-	{
-		System.out.println("______________________________________________________________________________________________________");
-		System.out.println("|                                        Welcome!                                                     |");
-		System.out.println("|                                                                                                     |");
-		System.out.println("|   A- Create New Contact                                                                             |");
-		System.out.println("|                                                                                                     |");
-		System.out.println("|                                                                                                     |");
-		System.out.println("|   B- Search                                                                                         |");
-		System.out.println("|                                                                                                     |");
-		System.out.println("|                                                                                                     |");
-		System.out.println("|   C- View all Contacts                                                                              |");
-		System.out.println("|                                                                                                     |");
-		System.out.println("|                                                                                                     |");
-		System.out.println("|                                                                                                     |");
-		System.out.println("|   D- Delete a Contact                                                                               |");
-		System.out.println("|                                                                                                     |");
-		System.out.println("|                                                                                                     |");
-		System.out.println("|   E - Save AddressBook to File                                                                      |");
-		System.out.println("|                                                                                                     |");
-		System.out.println("|                                                                                                     |");
-		System.out.println("|   F - EXIT                                                                                          |");
-		System.out.println("|                                                                                                     |");
-		System.out.println("_____________________________________________________________________________________________________");
-	}
+	
 }
