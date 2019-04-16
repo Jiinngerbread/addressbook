f85adb48701256eac10d593f60b082732a492821
blagu <49458613+blagu@users.noreply.github.com>
2019-04-15 20:38:00 -0500

    Create ComparebyEntryNum.java

---
+++ ComparebyEntryNum.java
@@ -0,0 +1,15 @@
+public class ComparebyEntryNum implements Comparator<Contact>
+{
+  public int compare(Contact con1, Contact con2)
+  {
+    Integer entryNum1 = con1.getEntry();
+    Integer entryNum2 = con2.getEntry();
+    
+    return entryNum1.compareTo(entryNum2);
+    
+  }
+
+
+
+
+}
