329ade4dda02cad279ca927f82dc778c2cd33ad3
Jiinngerbread <jiinngerbread@gmail.com>
2019-04-16 02:43:12 -0500

    Update DataManager.java

--- DataManager.java
+++ DataManager.java
@@ -1,36 +1,27 @@
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
+import java.lang.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.*;
-import java.util.*;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.nio.file.*;
+import java.nio.file.Files;
+
 
 /**
-* This class allows for the interaction of the Addressbook and User classes with their respective files. 
+* This class allows for the management of files (reading from, writing to, creating file).
 * @author Jheanel Brown and Esther E
-* @version 1.0
+* @version 2.0
 */
 public class DataManager
 {
 	private ArrayList <Contact> ContactList = new ArrayList<Contact>();
 	private String[] allInfo ;
-	
-	//private ArrayList <User> userList; 
-
-	//Check if there exists a folder in directory
-	// specific Users folder ie Addressbook user && specific user folder ie James 
-
-	// We want to create the create the Users folder
-	// We want to create a folder for a user in the Users folder 
-
-
-	// We want to check if a file exists
-
-	// We want to create a file in a specific destination folder 
-	// we want to write to the file 
-
-	//we want to edit the file /manipulate the file 
 
 	/**
 	* This allows for a User credentials to be verified by checking the file database for matching credentials
@@ -39,8 +30,8 @@ public class DataManager
 	**/
 	public boolean Authenticate(String username, String password)
 	{
-		try 
-		{	
+		try
+		{
 			FileReader fr = new FileReader("UserCredentials.txt");
 			BufferedReader br = new BufferedReader(fr);
 			String oneLine = "";
@@ -52,7 +43,7 @@ public class DataManager
 				//convert where necessary
 				String userN = eachLine[0];
 				String passW = eachLine[1];
-				
+
 				if(userN == username && passW == password)
 				{
 					return true;
@@ -71,7 +62,7 @@ public class DataManager
 
 	/**
 	* When called allows for a file to be written to
-	* 
+	*
 	*
 	**/
 	public void writeToFile(String data, String path)
@@ -98,15 +89,15 @@ public class DataManager
 
 	public ArrayList<Contacts> readFile(String username)
 	{
-		try 
-		{	
-			String filename = username+"_addressbook.txt" 
+		try
+		{
+			String filename = username+"_addressbook.txt"
 			FileReader fr = new FileReader(filename);
 			BufferedReader br = new BufferedReader(fr);
-			
+
 			String eachLine = "";
-			
-			
+
+
 			while ((eachLine = br.readLine()) != null)
 			{
 				//slit the line into tokens
@@ -119,14 +110,14 @@ public class DataManager
 				String contactPhones = allInfo[3];//needs to be CHAR and LONG
 				String contactEmails = allInfo[4];
 
-				//converting derived strings into more useful form 
+				//converting derived strings into more useful form
 
 				String[] contactData = contactInfo.split(",");
 				String firstN = contactData[0]; //needs to be string, string, gender, long
 				String lastN = contactData[1];
 				Gender gender = Gender.valueOf(contactData[2]).toUpperCase();
 				long dateOB = Long.parseLong(contactData[3]);
-				
+
 				//________________________
 				String[] phoneData = contactPhones.split(";");
 				String[] eachPhone = phoneData.split(",");
@@ -138,11 +129,11 @@ public class DataManager
 				Contact eachContact = new Contact(firstN, lastN, gender,dateOB);
 				eachContact.setAddress(contactAddress);
 				eachContact.setAlias(contactAlias);
-		
-				
+
+
 				for(String each: phoneData)
 				{
-					
+
 					String[] eachPhone = each.split(",");
 					char pType = each[0].charAt(0);
 					long pNumber = Long.parseLong(eachPhone[1]);
@@ -153,7 +144,7 @@ public class DataManager
 				{
 					eachContact.addEmail(i);
 				}
-				
+
 				ContactList.add(eachContact);
 			}
 			return ContactList;
@@ -168,7 +159,7 @@ public class DataManager
 			System.out.println("NOTE: the input has to be a number.");
 		}
 	}
-	
 
-	
+
+
 }
